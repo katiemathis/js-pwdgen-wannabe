46408374be9d29ef921d89f29be42cb6cf3c77c0
katiepalumbo <palumbo.katie@gmail.com>
2022-01-19 13:24:04 +0100

    esercizio completo

---
+++ main.js
@@ -0,0 +1,8 @@
+let nome = prompt("Inserisci il tuo nome");
+
+let cognome = prompt("Inserisci il tuo cognome");
+
+let color = prompt("Inserisci il tuo colore preferito");
+
+document.getElementById('password').innerHTML = "Your password is: " + nome + cognome + color;
+
